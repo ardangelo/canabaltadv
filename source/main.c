b1d29e3d5f538994ae17d284f518fb4ae4208fee
Andrew D'Angelo <excel@pharcyde.org>
2013-08-29 11:09:49 -0500

    Reorganization

--- source/main.c
+++ source/main.c
@@ -41,6 +41,24 @@ inline int inl_clamp(int x, int lo, int hi) {
 	return x;
 }
 
+inline unsigned int init_bg(
+	TILE* tile_mem_addr, const unsigned int* tiles, const unsigned int tileslen, 
+	const short unsigned int* map, int screenentrybegin, char charbaseblock) {
+
+	BLOCK *src, *dst0, *dst1;
+	int i;
+
+	memcpy(tile_mem_addr,tiles,tileslen);
+	src= (BLOCK*)map;
+	dst0= (BLOCK*)se_mem[screenentrybegin];
+	dst1= (BLOCK*)se_mem[screenentrybegin + 1];
+	for(i=0; i<32; i++) {
+		*dst0++= *src++;	 *dst0++= *src++;
+		*dst1++= *src++;	 *dst1++= *src++;
+	}
+	return BG_CBB(charbaseblock) | BG_SBB(screenentrybegin) | BG_4BPP | BG_REG_64x32;
+}
+
 void generate_building(struct BUILDING *building) {
 	building->style = BS1;
 	building->length++;
@@ -94,6 +112,7 @@ void game_loop() {
 	float p_v = 0;
 	u32 p_tid = 0, p_pb = 0;
 	int c_x = 0, c_y = 0;
+	u32 blend = 0x20;
 
 	struct BUILDING *current = malloc(sizeof(struct BUILDING));
 
@@ -111,6 +130,8 @@ void game_loop() {
 	obj_set_attr(player, ATTR0_TALL, ATTR1_SIZE_32, ATTR2_PALBANK(p_pb) | p_tid);
 	obj_set_pos(player, p_x, p_y);
 
+	REG_BG3VOFS = 64;
+
 	while(1) {
 		VBlankIntrWait();
 		key_poll();
@@ -118,10 +139,17 @@ void game_loop() {
 		if (vcount == 60)
 			vcount = 0;
 
+		if (blend) {
+			BFN_SET(REG_BLDCNT, 3, BLD_MODE);
+    		REG_BLDY= BLDY_BUILD(blend/2);
+    		blend--;
+		}
+
 		//animation
 		if (!(vcount % 2)) {//EWW MOD
 			p_tid += 8;
 		}
+
 		if (p_s == RUNNING) {
 			if (p_tid == RUNSTART+8*RUNFRAMES)
 				p_tid = RUNSTART;
@@ -144,8 +172,14 @@ void game_loop() {
 			p_v -= .5;
 		}
 		if (p_y > ground) {
-			p_s = ROLLING;
-			p_tid = ROLLSTART;
+			if (p_v < -7) {
+				p_s = ROLLING;
+				p_tid = ROLLSTART;
+			} else {
+				p_s = RUNNING;
+				p_tid = RUNSTART;
+			}
+			
 			p_y = ground;
 			p_v = 0;
 		}
@@ -183,9 +217,9 @@ void game_loop() {
 		REG_BG1HOFS = platforms_x;
 		REG_BG1VOFS = platforms_y + 64 - c_y;
 		REG_BG2HOFS = bg_x;
-		REG_BG2VOFS = (160-c_y)>>1;
+		REG_BG2VOFS = (256+128-32-c_y)>>2;
 		REG_BG3HOFS = bg_x>>1;
-		REG_BG3VOFS = (256-c_y)>>2;
+		//REG_BG3VOFS = (256-c_y)>>2;
 
 		player->attr2= ATTR2_BUILD(p_tid, p_pb, 0);
 		obj_set_pos(player, p_x, p_y + c_y);
@@ -201,53 +235,31 @@ void game_loop() {
 }
 
 int main() {
+
 	// Init interrupts and VBlank irq.
 	irq_init(NULL);
 	irq_add(II_TIMER1, kradInterrupt);
 	irq_add(II_VBLANK, vblank);
-	kragInit( KRAG_INIT_STEREO );					// init krawall
-	krapPlay( &mod_sanic, KRAP_MODE_LOOP, 0 );	// play module
 
-	BLOCK *src, *dst0, *dst1;
-	int i;
+	REG_BLDCNT = BLD_BUILD(BLD_OBJ | BLD_BG0 | BLD_BG1 | BLD_BG2 | BLD_BG3, BLD_BG3, 3);
+	BFN_SET(REG_BLDCNT, 3, BLD_MODE);
+    REG_BLDY= BLDY_BUILD(15);
 
-	REG_DISPCNT= DCNT_OBJ | DCNT_OBJ_1D | DCNT_MODE0 | DCNT_BG1 | DCNT_BG2 | DCNT_BG3;
+	REG_DISPCNT = DCNT_OBJ | DCNT_OBJ_1D | DCNT_MODE0 | DCNT_BG1 | DCNT_BG2 | DCNT_BG3;
 
 	memcpy(pal_bg_bank[0],SharedPal,SharedPalLen);
-	memcpy(&tile_mem[0][0],bg1Tiles,bg1TilesLen);
-	src= (BLOCK*)bg1Map;
-	dst0= (BLOCK*)se_mem[30];
-	dst1= (BLOCK*)se_mem[31];
-	for(i=0; i<32; i++) {
-		*dst0++= *src++;	 *dst0++= *src++;
-		*dst1++= *src++;	 *dst1++= *src++;
-	}
-	REG_BG2CNT = BG_CBB(0) | BG_SBB(30) | BG_4BPP | BG_REG_64x32;
-
-	memcpy(&tile_mem[1][0],bg2Tiles,bg2TilesLen);
-	src= (BLOCK*)bg2Map;
-	dst0= (BLOCK*)se_mem[28];
-	dst1= (BLOCK*)se_mem[29];
-	for(i=0; i<32; i++) {
-		*dst0++= *src++;	 *dst0++= *src++;
-		*dst1++= *src++;	 *dst1++= *src++;
-	}
-	REG_BG3CNT = BG_CBB(1) | BG_SBB(28) | BG_4BPP | BG_REG_64x32;
+	REG_BG2CNT = init_bg(&tile_mem[0][0],bg1Tiles,bg1TilesLen,bg1Map,30,0);
+	REG_BG3CNT = init_bg(&tile_mem[1][0],bg2Tiles,bg2TilesLen,bg2Map,28,1);
 
 	memcpy(pal_bg_bank[1],platformsPal,platformsPalLen);
-	se_mem[26][0] = SE_PALBANK(1);
-	memcpy(&tile_mem[2][0],platformsTiles,platformsTilesLen);
-	src= (BLOCK*)platformsMap;
-	dst0= (BLOCK*)se_mem[26];
-	dst1= (BLOCK*)se_mem[32];	//might have to dma this when it gets to dynamic
-	for(i=0; i<32; i++) {
-		*dst0++= *src++;	 *dst0++= *src++;
-		*dst1++= *src++;	 *dst1++= *src++;
-	}
-	REG_BG1CNT = BG_CBB(2) | BG_SBB(26) | BG_4BPP | BG_REG_64x32;
+	se_mem[26][0] = SE_PALBANK(1); //a sad attempt at solving the palette problem
+	REG_BG1CNT = init_bg(&tile_mem[2][0],platformsTiles,platformsTilesLen,platformsMap,26,2);
 
 	memcpy(&tile_mem[4][0], playerTiles, playerTilesLen);
 	memcpy(pal_obj_mem, playerPal, playerPalLen);
+
+	kragInit( KRAG_INIT_STEREO );					// init krawall
+	krapPlay( &mod_sanic, KRAP_MODE_LOOP, 0 );	// play module
 	
 	game_loop();
 
