f1fd58f81997f18e6e1d409b3cd1f45de140a7e7
Andrew D'Angelo <excel@pharcyde.org>
2014-01-18 11:06:45 -0600

    Started rework of building display
    Still need to fix building generation
    Writing map to se_mem now generalized in anticipation for DMA code
    append_building now draw_building, allowing for custom start and end
    values

--- source/main.c
+++ source/main.c
@@ -30,10 +30,12 @@ inline int randint(int min, int max) {
 
 inline void set_dtile(int x, int y, int offset) {
 
-	platformsMap[2*x+64*(y)] = offset;
-	platformsMap[2*x+1+64*(y)] = offset+1;
-	platformsMap[2*x+64*(y+1)] = offset+2;
-	platformsMap[2*x+1+64*(y+1)] = offset+3;
+	//(0,0) is at bottom left of screen now
+
+	platformsMap[2*x +   MAP_LENGTH * (MAP_HEIGHT -  2*y     )] = offset;
+	platformsMap[2*x + 1+MAP_LENGTH * (MAP_HEIGHT -  2*y     )] = offset+1;
+	platformsMap[2*x +   MAP_LENGTH * (MAP_HEIGHT - (2*y - 1))] = offset+2;
+	platformsMap[2*x + 1+MAP_LENGTH * (MAP_HEIGHT - (2*y - 1))] = offset+3;
 }
 
 inline int inl_clamp(int x, int lo, int hi) {
@@ -51,29 +53,35 @@ inline void score_to_tid(u32 *tid_array, int score) {
 	}
 }
 
-inline unsigned int init_bg(
-	TILE* tile_mem_addr, const unsigned int* tiles, const unsigned int tileslen, 
-	const short unsigned int* map, int screenentrybegin, char charbaseblock) {
-
-	BLOCK *src, *dst0, *dst1;
+inline void updateVRAMFromMap(unsigned short* mapPtr, int bank0, int bank1) {
 	int i;
+	//might need to move this to DMA
+	BLOCK *src, *dst0, *dst1;
+	src= (BLOCK*)mapPtr;
+	dst0= (BLOCK*)se_mem[bank0];
+	dst1= (BLOCK*)se_mem[bank1];
 
-	memcpy(tile_mem_addr,tiles,tileslen);
-	src= (BLOCK*)map;
-	dst0= (BLOCK*)se_mem[screenentrybegin];
-	dst1= (BLOCK*)se_mem[screenentrybegin + 1];
 	for(i=0; i<32; i++) {
 		*dst0++= *src++;	 *dst0++= *src++;
 		*dst1++= *src++;	 *dst1++= *src++;
-	}
+	}	
+}
+
+inline unsigned int init_bg(
+	TILE* tile_mem_addr, const unsigned int* tiles, const unsigned int tileslen, 
+	const short unsigned int* map, int screenentrybegin, char charbaseblock) {
+	memcpy(tile_mem_addr,tiles,tileslen);
+
+	updateVRAMFromMap(map, screenentrybegin, screenentrybegin+1);
+
 	return BG_CBB(charbaseblock) | BG_SBB(screenentrybegin) | BG_4BPP | BG_REG_64x32;
 }
 
 void generate_building(struct BUILDING *building) {
 	building->style = BS1;
-	building->length = randint(11,13);
-	building->height = randint(6,9);
-	building->gap = randint(3,6);
+	building->length = LEN_MAX; //randint(LEN_MIN,LEN_MAX);
+	building->height = HEIGHT_MAX; //randint(HEIGHT_MIN,HEIGHT_MAX);
+	building->gap = GAP_MAX; //randint(GAP_MIN,GAP_MAX);
 }
 
 void clear_buildings() {
@@ -81,33 +89,44 @@ void clear_buildings() {
 	memset(platformsMap,0,sizeof(platformsMap));
 }
 
-void append_building(struct BUILDING *building, int offset) {
-	int i = 0, j = 0;
+/* drawStart specifies the point on the theoretical building to start drawing at offset, likewise for drawStop but for ending
+*/
+void draw_building(struct BUILDING *building, int locationOffset, int drawStart, int drawStop, int drawEdge) {
+	int i,j;
 
-	set_dtile(offset, 32-2*building->height, building->style + TL);
-	for (i = 1;i<building->length;i++) {
-		set_dtile(offset+i, 32-2*building->height, building->style + TE);
+	//if drawstart == 0
+	if (drawStart == 0) {
+		//draw top left corner
+		set_dtile(locationOffset, building->height, building->style+TL);
 	}
-	set_dtile(offset+building->length, 32-2*building->height, building->style + TR);
-	for (i = 1;i<building->height;i++) {
-		set_dtile(offset, 32-2*building->height+2*i, building->style + LE);
-		for (j = 1;j<building->length;j++) {
-			set_dtile(offset+j, 32-2*building->height+2*i, building->style + MI);
+	//for drawstart+1 to drawstop
+	for (i = 1; i < drawStop-drawStart; i++) {
+		//draw top edge
+		set_dtile(locationOffset+i, building->height, building->style+TE);
+	}
+	//if drawedge draw top right corner
+	if (drawEdge) {
+		set_dtile(locationOffset+(drawStop-drawStart), building->height, building->style+TR);
+	}
+	//for 1 to height
+	for (i = 1; i < building->height; i++) {
+		//if drawstart == 0
+		if (drawStart == 0) {
+			//draw left edge
+			set_dtile(locationOffset, i, building->style+LE);
 		}
-		set_dtile(offset+building->length, 32-2*building->height+2*i, building->style + RE);
+		//for drawstart+1 to drawstop
+		for (j = 1; j < drawStop-drawStart; j++) {
+			//draw middle block
+			set_dtile(locationOffset+j, i, building->style+MI);
+		}
+		//if drawedge draw right edge block
+		if (drawEdge) {
+			set_dtile(locationOffset+(drawStop-drawStart), i, building->style+RE);
+		}
 	}
 
-	//might need to move this to DMA
-	BLOCK *src, *dst0, *dst1;
-	src= (BLOCK*)platformsMap;
-	dst0= (BLOCK*)se_mem[26];
-	dst1= (BLOCK*)se_mem[27];
-
-	for(i=0; i<32; i++) {
-		*dst0++= *src++;	 *dst0++= *src++;
-		*dst1++= *src++;	 *dst1++= *src++;
-	}	
-	
+	updateVRAMFromMap(platformsMap, 26, 27);
 }
 
 void vblank() {
@@ -122,10 +141,7 @@ void vblank() {
 
 void game_loop() {
 	int i = 0, vcount = 0, bg_x = 0, ground;
-	int p_x = 10, p_y = 0, p_s = RUNNING;
-	float p_v = 0;
-	u32 p_tid = 0, p_pb = 0;
-	int c_x = 0, c_y = 0;
+
 	u32 blend = 0x20;
 	u32 score = 0;
 	u32 digits_tid[SCORE_LEN];
@@ -133,28 +149,36 @@ void game_loop() {
 	struct BUILDING *current = malloc(sizeof(struct BUILDING));
 	struct BUILDING *next = malloc(sizeof(struct BUILDING));
 
+	struct PLAYER *player = malloc(sizeof(struct PLAYER));
+	player->x = 16; player->y = 0; player->state = RUNNING; //player intial state
+	player->vel = 0;
+	player->tid = 0; player->palbank = 0; player->jumptime = 0;
+
+	struct CAMERA *camera = malloc(sizeof(struct CAMERA));
+	camera->x = 0; camera->y = 0; camera->vel = 3;
+
 	int platforms_x = 0, platforms_y = 0;
 	
 	oam_init(obj_buffer, 128);
 	
 	generate_building(current);
-	append_building(current, 0);
+	draw_building(current, 0, 0, current->length, 1);
 	ground = 256-current->height*16 - 96;
 	generate_building(next);
-	append_building(next, current->length + current->gap);
+	draw_building(next, current->length + current->gap, 0, ENOUGH_BUILDING, 0);
 	
-	OBJ_ATTR *player = &obj_buffer[0];
-	obj_set_attr(player, ATTR0_TALL, ATTR1_SIZE_32, ATTR2_PALBANK(p_pb) | p_tid);
-	obj_set_pos(player, p_x, p_y);
+	OBJ_ATTR *player_obj = &obj_buffer[0];
+	obj_set_attr(player_obj, ATTR0_TALL, ATTR1_SIZE_32, ATTR2_PALBANK(player->palbank) | player->tid);
+	obj_set_pos(player_obj, player->x, player->y);
 
 	OBJ_ATTR *meter = &obj_buffer[1];
-	obj_set_attr(meter, ATTR0_TALL, ATTR1_SIZE_8, ATTR2_PALBANK(p_pb) | (316));
+	obj_set_attr(meter, ATTR0_TALL, ATTR1_SIZE_8, ATTR2_PALBANK(player->palbank) | (316));
 	obj_set_pos(meter, (240-8*(SCORE_LEN+1))+8*SCORE_LEN, 0);
 
 	OBJ_ATTR *digits[SCORE_LEN];
 	for (i = 0;i<SCORE_LEN;i++) {
 		digits[i] = &obj_buffer[i+2];
-		obj_set_attr(digits[i], ATTR0_TALL, ATTR1_SIZE_8, ATTR2_PALBANK(p_pb) | digits_tid[i]);
+		obj_set_attr(digits[i], ATTR0_TALL, ATTR1_SIZE_8, ATTR2_PALBANK(player->palbank) | digits_tid[i]);
 		obj_set_pos(digits[i], (240-8*(SCORE_LEN+1))+8*i, 0);
 	}
 
@@ -173,112 +197,127 @@ void game_loop() {
     		blend--;
 		}
 
-		//check for ground
-		if (current->length*16 < platforms_x + (p_x+12)) {
+		//check for ground and first boundary
+		if (current->length*16 < platforms_x + (player->x)) { //in between buildings
 			ground = 256;
+			//unload map
+			//clear_buildings();
+			//reset platforms_x to player_x
+			//platforms_x = -(player->x+8);
+			//draw enough of current building
+			//fill rest of map with next building
 		}
 
+		//boundary 2: enter next
 		if (ground == 256) {
-			if ((current->length*16 + current->gap*16 - platforms_x) < p_x+8) {
+			if ((current->length*16 + current->gap*16 - platforms_x) < player->x+8) {
 
-				if (p_y - 16 > 256-next->height*16 - 96) { //16 for lenience
-					p_s = FALLING_TO_DEATH;
+				if (player->y - 16 > 256-next->height*16 - 96) { //16 for lenience
+					player->state = FALLING_TO_DEATH;
 				}
 
-				//clear buildings and reset platforms_x to p_x
+				//clear buildings and reset platforms_x to player->x
 				clear_buildings();
-				platforms_x = -(p_x+8);
+				platforms_x = -(player->x+8);
 				//current = next
 				current = next;
 				//reset ground
 				ground = 256-current->height*16 - 96;
 				//place current at beginning
-				append_building(current,0);
+				draw_building(current, 0, 0, current->length, 1);
 				//generate next
 				generate_building(next);
 				//place next
-				append_building(next, current->length + current->gap);
+				draw_building(next, current->length + current->gap, 0, ENOUGH_BUILDING, 0);
 			}
+
 		}
 
-		if (p_y > 200-16) {
+		if (player->y > 200-16) {
 			swi_call(0x0);
 		}
 
 		//key handling
-		c_x += 3*key_tri_horz();
-		c_x = inl_clamp(c_y, 0, 4*256);
-		c_y -= 3*key_tri_vert();
-		c_y = inl_clamp(c_y, -32, 64);
+		camera->x += 3*key_tri_horz();
+		camera->x = inl_clamp(camera->x, 0, 4*256);
+		camera->y -= 3*key_tri_vert();
+		camera->y = inl_clamp(camera->y, -32, 64);
 		if (key_released(KEY_SELECT)) {
-			p_y = -50;
+			player->y = -50;
 			generate_building(current);
-			append_building(current, 0);
+			draw_building(current, 0, 0, current->length, 1);
 			ground = 256-current->height*16 - 96;
 		}
-		if (p_s == RUNNING || p_s == ROLLING) {
+		if ((player->state == RUNNING || player->state == ROLLING) && player->vel >= -3) {
 			if (key_hit(KEY_A)) {
-				p_s = JUMPING;
-				p_tid = JUMPSTART;
-				p_v = 6;
+				player->state = JUMPING;
+				player->tid = JUMPSTART;
+				player->vel = 5;
+				player->jumptime = 0;
 			}
-		} else if (p_s == JUMPING) {
+		} else if (player->state == JUMPING) {
 			//variable building->height jumping
+			if (key_is_down(KEY_A) && player->jumptime < MAXJUMPTIME) {
+				player->vel = 5;
+				player->jumptime++;
+			}
 		}
 
 		//animation
-		score_to_tid(digits_tid,score);
+		score_to_tid(digits_tid,current->length); //for debug display enter variable here
 
-		if (!(vcount % 2)) {//EWW MOD
-			p_tid += 8;
-		}
-		
-		score += 3;
-		if (p_s != FALLING_TO_DEATH) {
-			platforms_x += 3;
-			if (platforms_x > 512) {
-				platforms_x = 0;
+		if (!(vcount & 0x1)) {//mod has been removed
+			player->tid += 8;
+			if (player->state == FALLING_TO_DEATH) {
+				if (camera->vel)
+					camera->vel = 0;
 			}
 		}
+		
+		score += (int)(camera->vel/2);
+		platforms_x += camera->vel;
+		if (platforms_x > 512) {
+			platforms_x = 0;
+		}
 
-		if (p_s == RUNNING) {
-			if (p_tid == RUNSTART+8*RUNFRAMES)
-				p_tid = RUNSTART;
-		} else if (p_s == JUMPING) { //can your programming language do this?
-			if (p_tid == JUMPSTART+8*JUMPFRAMES)
-				p_tid = JUMPSTART;
-		} else if (p_s == FALLING) {
-			if (p_tid == FALLSTART+8*FALLFRAMES)
-				p_tid = FALLSTART;
-		} else if (p_s == ROLLING) {
-			if (p_tid == ROLLSTART+8*ROLLFRAMES) {
-				p_s = RUNNING;
-				p_tid = RUNSTART;
+		if (player->state == RUNNING) {
+			if (player->tid == RUNSTART+8*RUNFRAMES)
+				player->tid = RUNSTART;
+		} else if (player->state == JUMPING) { //can your programming language do this?
+			if (player->tid == JUMPSTART+8*JUMPFRAMES)
+				player->tid = JUMPSTART;
+		} else if (player->state == FALLING) {
+			if (player->tid == FALLSTART+8*FALLFRAMES)
+				player->tid = FALLSTART;
+		} else if (player->state == ROLLING) {
+			if (player->tid == ROLLSTART+8*ROLLFRAMES) {
+				player->state = RUNNING;
+				player->tid = RUNSTART;
 			}
 		}
 
 		//movement
-		p_y -= p_v;
-		if (p_y != ground) {
-			p_v -= .5;
+		player->y -= player->vel;
+		if (player->y != ground) {
+			player->vel -= .5;
 		}
-		if (p_s != FALLING_TO_DEATH) {
-			if (p_y > ground) {
-				if (p_v < -7) {
-					p_s = ROLLING;
-					p_tid = ROLLSTART;
+		if (player->state != FALLING_TO_DEATH) {
+			if (player->y > ground) {
+				if (player->vel < -9) {
+					player->state = ROLLING;
+					player->tid = ROLLSTART;
 				} else {
-					p_s = RUNNING;
-					p_tid = RUNSTART;
+					player->state = RUNNING;
+					player->tid = RUNSTART;
 				}
 				
-				p_y = ground;
-				p_v = 0;
+				player->y = ground;
+				player->vel = 0;
 			}
 		}
-		if (p_v < 0 && p_s == JUMPING) {
-			p_s = FALLING;
-			p_tid = FALLSTART;
+		if (player->vel < 0 && player->state == JUMPING) {
+			player->state = FALLING;
+			player->tid = FALLSTART;
 		}
 
 		bg_x++;
@@ -286,20 +325,20 @@ void game_loop() {
 			bg_x = 0;
 
 		REG_BG1HOFS = platforms_x;
-		REG_BG1VOFS = platforms_y + 64 - c_y;
+		REG_BG1VOFS = platforms_y + 64 - camera->y;
 		REG_BG2HOFS = bg_x;
-		REG_BG2VOFS = (256+128-32-c_y)>>2;
+		REG_BG2VOFS = (256+128-32-camera->y)>>2;
 		REG_BG3HOFS = bg_x>>1;
-		//REG_BG3VOFS = (256-c_y)>>2;
+		//REG_BG3VOFS = (256-camera->y)>>2;
 
-		player->attr2= ATTR2_BUILD(p_tid, p_pb, 0);
-		obj_set_pos(player, p_x, p_y + c_y);
+		player_obj->attr2= ATTR2_BUILD(player->tid, player->palbank, 0);
+		obj_set_pos(player_obj, player->x, player->y + camera->y);
 
-		meter->attr2= ATTR2_BUILD(316, p_pb, 0);
+		meter->attr2= ATTR2_BUILD(316, player->palbank, 0);
 		obj_set_pos(meter, (240-8*(SCORE_LEN+1))+8*SCORE_LEN, 0);
 
 		for (i = 0;i<SCORE_LEN;i++) {
-			digits[i]->attr2= ATTR2_BUILD(digits_tid[i], p_pb, 0);
+			digits[i]->attr2= ATTR2_BUILD(digits_tid[i], player->palbank, 0);
 			obj_set_pos(digits[i], (240-8*(SCORE_LEN+1))+8*i, 0);
 		}
 		oam_copy(oam_mem, obj_buffer, 2+SCORE_LEN); 
--- source/main.h
+++ source/main.h
@@ -1,3 +1,4 @@
+//animation start frames
 #define RUNNING 0
 #define RUNSTART 0
 #define RUNFRAMES 16
@@ -16,10 +17,22 @@
 
 #define FALLING_TO_DEATH 8
 
-#define MAXJUMPVEL 12
-
+//settings
+#define MAXJUMPTIME 10
 #define SCORE_LEN 8
+#define MAP_HEIGHT 32
+#define MAP_LENGTH 64
 
+#define LEN_MIN 7
+#define LEN_MAX 13//32dT - enoughdT - gapmaxdT
+#define HEIGHT_MIN 6
+#define HEIGHT_MAX 9
+#define GAP_MIN 4
+#define GAP_MAX 6
+#define ENTIRE_BUILDING 0
+#define ENOUGH_BUILDING 10//32dT - space before player in tiles. if we ever switch to camera movement on x plane this must be adjusted
+
+//building tile style data
 #define BS1 8
 
 #define TL 12
@@ -34,6 +47,16 @@ struct BUILDING {
 	int length;
 	int height;
 	int gap;
+};
+
+struct PLAYER { //add obj_attr to this eventually
+	int x, y, state;
+	float vel;
+	u32 tid, palbank, jumptime;
+};
+
+struct CAMERA {
+	int x, y, vel;
 };
 
 //grit bg1.bmp bg2.bmp -gTff00ff -gB4 -mRtpf -pS -ftc
