dd381c1a269e4271c6b42dc8060aa4e562b8ecea
Andrew D'Angelo <dangeloandrew@outlook.com>
2017-01-07 17:51:25 -0600

    crappy infinite scroller

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include "tiles/bg.h"
 #include "tiles/sprites.h"
 
-#define SCR_AT(x,y) ((((x%64 > 32) ? 32*32 : 0) + (32*y) + x%32) % (64*32))
+#define SCR_AT(x,y) ((((x%64 >= 32) ? 32*32 : 0) + (32*y) + x%32) % (64*32))
 
 int main(void) {
 
@@ -16,16 +16,15 @@ int main(void) {
 	// use block 0 as charblock, 30/31 as screenblocks. 4 bits per pixel, 64x32 tiles
 	memcpy(&tile_mem[0][0], bgTiles, bgTilesLen); // load tiles into charblocks
 	SCR_ENTRY *bg0_map = se_mem[30];
-	for (int i = 0; i < 32; i++) { // fill in top 32 tiles
-		bg0_map[SCR_AT(i,i)] = SE_PALBANK(0) | TILE_BUILD_S0;
+	for (int i = 0; i <= (SCREEN_WIDTH >> 3); i++) { // fill in the initial map
+		bg0_map[SCR_AT(i, (int)(rand() % 16))] = SE_PALBANK(0) | TILE_BUILD_S0;
 	}
 	REG_BG0CNT = BG_CBB(0) | BG_SBB(30) | BG_4BPP | BG_REG_64x32; // apply bg settings
-	
 	REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
 
 	// Scroll around some
 	int x = 0;
-	int horizon = 32;
+	int horizon = (SCREEN_WIDTH >> 3);
 	while(1) {
 		vid_vsync();
 		key_poll();
@@ -33,9 +32,12 @@ int main(void) {
 		int changed = key_tri_horz();
 		x += changed;
 		if (changed > 0) {
-			int new_col = ((x + SCREEN_WIDTH) >> 3);
-			if (new_col >= horizon) {
-				bg0_map[SCR_AT(new_col, 1)] = SE_PALBANK(0) | TILE_BUILD_S0;
+			int new_col = ((x + SCREEN_WIDTH) >> 3) + 1;
+			if (new_col > horizon) {
+				int h = rand() % 16;
+				for (int i = 0; i < 16; i++) {
+					bg0_map[SCR_AT(new_col, i)] = SE_PALBANK(0) | ((i == h)?TILE_BUILD_S0:TILE_SKY_S0);
+				}
 				horizon = new_col;
 			}
 		}
